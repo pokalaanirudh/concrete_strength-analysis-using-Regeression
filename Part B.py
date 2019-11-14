6532a68156eb00e48ca4ea7bb83a5f0ac7fc7b31
pokalaanirudh <35173579+pokalaanirudh@users.noreply.github.com>
2019-11-14 22:15:17 +0530

    Create Part B.py

---
+++ Part B.py
@@ -0,0 +1,59 @@
+import keras
+
+from keras.models import Sequential
+from keras.layers import Dense
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+import statistics
+import numpy as np
+
+dataframes = pd.read_csv("/home/pokala/coursera/concrete_data.csv")
+mean_sq_er_list=[]   # list for mean squared error
+
+
+def model():
+    
+    dataset= dataframes.values
+    predictor = dataset[:,0:8]
+    target    = dataset[:,8]
+  
+    norm_predictor = (predictor - predictor.mean()) / predictor.std()
+    
+    predictor_train , predictor_test , target_train , target_test = train_test_split(norm_predictor , target , test_size=0.30)
+    n_cols= predictor.shape[1]    # the shape of input layer
+    
+    
+    # model of neural network
+    model = Sequential()
+    model.add(Dense(10,activation="relu",input_shape=(n_cols,)))
+    model.add(Dense(1))
+    
+    # training and testing the model 
+    model.compile(optimizer="adam", loss="mean_squared_error")
+    model.fit(predictor_train,target_train,epochs=50)
+    result = model.predict(predictor_test)
+    
+    # for computing the mean squared error between predicted and actual value
+    x=mean_squared_error(target_test,result) 
+    mean_sq_er_list.append(x)   
+    
+    
+    # for repeating the step 1,2,3 50 times
+for i in range(50):
+    model()
+    
+# the final mean and standard deviation of the 50 mean squared error 
+
+mean_B= statistics.mean(mean_sq_er_list)
+
+standard_deviation_B = statistics.stdev(mean_sq_er_list)
+
+mean_A = 273.18725491088185
+mean_difference = mean_B - mean_A
+mean_difference= 17.379914081360425
+
+standard_deviation_B=40.30688302676195
+
+mean_B=290.56716899224227
+    
